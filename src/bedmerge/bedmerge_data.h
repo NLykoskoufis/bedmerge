dcb522d260aeb4d53bf5316edf1e0509e5422d93
NLykoskoufis <nikolaslykos94@gmail.com>
2022-09-27 10:19:04 +0300

    First commit of the project

---
+++ src/bedmerge/bedmerge_data.h
@@ -0,0 +1,30 @@
+#pragma once 
+
+#include "../common/data.h"
+
+
+class Bedmerge {
+public: 
+
+    // GTF related 
+    std::vector < std::string > ensemblIDvector;
+    std::vector < std::string > geneNameVector;
+    std::vector < std::string > gtfChrVector;
+    std::vector < int > gtfStartVector; 
+    std::vector < int > gtfEndVector;
+    std::vector < std::string > gtfStrandVector;
+
+    std::unordered_map < std::string, unsigned int > gtfGeneIndexMap; 
+
+
+    // BED RELATED 
+
+    std::vector < double > 
+
+
+    // FUNCTIONS 
+
+
+
+
+};
